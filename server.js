64391d547723f2ff48a74aac346c2f12edf6d70c
Mustafa Çakmak <57947073+xmcakmak@users.noreply.github.com>
2023-09-19 17:14:58 +0300

    (fix): env file bug fixed

--- server.js
+++ server.js
@@ -1,62 +1,62 @@
-const express = require("express");
-const cors = require("cors");
-const cookieSession = require("cookie-session");
+const express = require("express")
+const cors = require("cors")
+const cookieSession = require("cookie-session")
+require('dotenv').config();
 
-const app = express();
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
 
-app.use(cors());
+const app = express()
 
-// parse requests of content-type - application/json
-app.use(express.json());
+app.use(cors())
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
 
 app.use(
-  cookieSession({
-    name: "resortcode-session",
-    keys: ["COOKIE_SECRET"], // should use as secret environment variable
-    httpOnly: true,
-  })
-);
+	cookieSession({
+		name: "resortcode-session",
+		keys: ["COOKIE_SECRET"],
+		httpOnly: true,
+	}),
+)
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to resortcode application." });
-});
+	res.json({ message: "Welcome to resortcode application." })
+})
 
-
-const db = require("./models");
-const Role = db.role;
+const db = require("./models")
+const Role = db.role
 
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync Db');
 //   initial();
 // });
 
-require("./routes/auth.routes")(app);
-require("./routes/user.routes")(app);
-
+require("./routes/auth.routes")(app)
+require("./routes/user.routes")(app)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+	console.log(`Server is running on port ${PORT}.`)
+})
 
 function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  });
- 
-  Role.create({
-    id: 2,
-    name: "moderator"
-  });
- 
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
+	Role.create({
+		id: 1,
+		name: "user",
+	})
+
+	Role.create({
+		id: 2,
+		name: "moderator",
+	})
+
+	Role.create({
+		id: 3,
+		name: "admin",
+	})
 }
